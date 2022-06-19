203218c5ef21c1aa8a2f80f23f5ff0151e904a74
Ellonanew <ehllona@icloud.com>
2022-06-20 01:09:57 +0300

    complete module

--- 3-module/2-task/index.js
+++ 3-module/2-task/index.js
@@ -1,3 +1,16 @@
 function filterRange(arr, a, b) {
-  // ваш код...
+  let newAr = [], min, max;
+  if (a < b){
+    min = a;
+    max = b;
+  } else {
+    min = b;
+    max = a;
+  }
+  arr.forEach((e) => {
+    if (e >= min && e <= max) {
+      newAr.push(e)
+    }
+  })
+  return newAr
 }
--- 3-module/5-task/index.js
+++ 3-module/5-task/index.js
@@ -1,3 +1,8 @@
 function getMinMax(str) {
-  // ваш код...
+  let arr = str.split(" ");
+  let result = arr.filter((e) => {
+    return Number(e);
+  }).map((e) => Number(e));
+  result.sort((a, b) => {return a - b});
+  return {min: result[0], max: result[result.length - 1]}
 }
