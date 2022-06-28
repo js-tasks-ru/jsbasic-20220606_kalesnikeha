7f8493e8e9a7f4b6067bdb61ff01e190a328efdf
Ellonanew <ehllona@icloud.com>
2022-06-28 23:57:41 +0300

    complete module 4

--- 4-module/3-task/index.js
+++ 4-module/3-task/index.js
@@ -1,3 +1,17 @@
 function highlight(table) {
-  // ваш код...
+  for (let row of table.rows) {
+
+    if (row.cells[3].dataset) {
+      let dsClass = row.cells[3].dataset.available === 'true' ? "available" : "unavailable";
+      row.classList.add(dsClass);
+    } else {
+      row.hidden = true;
+    }
+
+    let genClass = row.cells[2].innerText === 'm' ? 'male' : 'female';
+    row.classList.add(genClass);
+
+    if (parseInt(row.cells[1].innerText) < 18)
+      row.style.textDecoration = 'line-through';
+  }
 }
