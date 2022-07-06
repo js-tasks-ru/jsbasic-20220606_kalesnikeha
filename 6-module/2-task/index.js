786d99c8fc34d6029f2a798bf4ae235548b4f3bc
Ellonanew <ehllona@icloud.com>
2022-07-06 21:34:27 +0300

    complete module 6

--- 6-module/2-task/index.js
+++ 6-module/2-task/index.js
@@ -1,4 +1,26 @@
+import createElement from '../../assets/lib/create-element.js';
+
 export default class ProductCard {
   constructor(product) {
+    const item = createElement(`<div class="card">
+                                        <div class="card__top">
+                                            <img src="/assets/images/products/${product.image}" class="card__image" alt="product">
+                                            <span class="card__price">€${product.price.toFixed(2)}</span>
+                                        </div>
+                                        <div class="card__body">
+                                            <div class="card__title">${product.name}</div>
+                                            <button type="button" class="card__button">
+                                                <img src="/assets/images/icons/plus-icon.svg" alt="icon">
+                                            </button>
+                                        </div>
+                                    </div>`);
+    this.elem = item;
+    const event = new CustomEvent("product-add", {
+      detail: product.id,
+      bubbles: true
+    });
+    this.elem.querySelector('.card__button').onclick = function () {
+      item.dispatchEvent(event);
+    };
   }
 }
--- 6-module/3-task/index.js
+++ 6-module/3-task/index.js
@@ -3,5 +3,72 @@ import createElement from '../../assets/lib/create-element.js';
 export default class Carousel {
   constructor(slides) {
     this.slides = slides;
+    this.offset = 0;
+    this.elem = createElement(`<div class="carousel"></div>`);
+    this.left = createElement(`<div class="carousel__arrow carousel__arrow_left"><img src="/assets/images/icons/angle-left-icon.svg" alt="icon"></div>`);
+    this.right = createElement(`<div class="carousel__arrow carousel__arrow_right"><img src="/assets/images/icons/angle-icon.svg" alt="icon"></div>`);
+    this.elem.appendChild(this.right);
+    this.elem.appendChild(this.left);
+    this.slider = document.createElement('div');
+    this.slider.classList.add('carousel__inner');
+    document.addEventListener('toggle_slide', (event) => {
+      if (event.detail === 'left') {
+        this.right.style.display = 'flex';
+        if (this.offset === 0) {
+          this.left.style.display = 'none';
+        }
+        this.slider.style.transform = 'translateX(' + this.offset + 'px)';
+      } else {
+        this.left.style.display = 'flex';
+        if (this.offset === -(this.slideWidth * (this.slides.length - 1))) {
+          this.right.style.display = 'none';
+        }
+        this.slider.style.transform = 'translateX(' + this.offset + 'px)';
+      }
+    });
+
+    this.left.onclick = () => this.leftClick();
+    this.left.style.display = 'none';
+
+    this.right.onclick = () => this.rightClick();
+
+    this.elem.appendChild(this.slider);
+    this.slides.forEach(el => {
+      let slide = createElement(`<div class="carousel__slide" data-id="${el.id}">
+                                        <img src="/assets/images/carousel/${el.image}" class="carousel__img" alt="slide">
+                                        <div class="carousel__caption">
+                                          <span class="carousel__price">€${el.price.toFixed(2)}</span>
+                                          <div class="carousel__title">${el.name}</div>
+                                          <button type="button" class="carousel__button">
+                                            <img src="/assets/images/icons/plus-icon.svg" alt="icon">
+                                          </button>
+                                        </div>
+                                      </div>`);
+      slide.querySelector('.carousel__button').onclick = e => {
+        this.elem.dispatchEvent(new CustomEvent("product-add", {
+          detail: el.id,
+          bubbles: true
+        }));
+      };
+      this.slider.appendChild(slide);
+    });
+  }
+
+  leftClick() {
+    this.slideWidth = this.elem.getElementsByClassName('carousel__slide')[0].offsetWidth;
+    this.offset += this.slideWidth;
+    this.elem.dispatchEvent(new CustomEvent("toggle_slide", {
+      detail: "left",
+      bubbles: true
+    }));
+  }
+
+  rightClick() {
+    this.slideWidth = this.elem.getElementsByClassName('carousel__slide')[0].offsetWidth;
+    this.offset -= this.slideWidth;
+    this.elem.dispatchEvent(new CustomEvent("toggle_slide", {
+      detail: "right",
+      bubbles: true
+    }));
   }
 }
